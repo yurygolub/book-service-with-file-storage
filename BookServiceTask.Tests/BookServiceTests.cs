0280995f4af7b5cf9d38fbfc3a5f295bc6740adc
Anzhelika Kravchuk <AnzhelikaKravchuk@users.noreply.github.com>
2021-12-02 15:26:21 +0300

    Create empty solution.

---
+++ BookServiceTask.Tests/BookServiceTests.cs
@@ -0,0 +1,10 @@
+using NUnit.Framework;
+
+namespace BookServiceTask.Tests
+{
+    [TestFixture]
+    public class BookServiceTests
+    {
+        //TODO: Add test methods here. Remove this comment.
+    }
+}
---
+++ BookServiceTask/BookService.cs
@@ -0,0 +1,7 @@
+﻿namespace BookServiceTask
+{
+    public class BookService
+    {
+        //TODO: Add necessary code here. Remove this comment. 
+    }
+}
